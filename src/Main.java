b3e6ada5889b41b780f7d2bddb11419ba2e7e503
adamd <adam.d.hinton@gmail.com>
2024-02-22 19:59:29 -0500

    working solution. Yay!

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,75 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        SalariedEmployee bob = new SalariedEmployee("Bob", "04/20/1993", "02/22/2024", 234902422,"02/22/2021",
+                43242432, true );
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        bob.retire();
+    }
+}
+
+class Worker{
+    protected String name;
+    private String birthDate;
+    private String endDate;
+
+    public Worker(String name, String birthDate, String endDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.endDate = endDate;
+    }
+
+    public int getAge(){
+        return 56;
+    }
+
+    protected String getName(){
+        return this.name;
+    }
+
+    public double collectPay(){
+        return 2000;
+    }
+
+    public void terminate(String endDate){
+        System.out.println("Terminated " + endDate);
+    }
+}
+
+
+
+
+class Employee extends Worker {
+    private long employeeId;
+    private String hireDate;
+
+    public Employee(String name, String birthDate, String endDate, long employeeId, String hireDate) {
+        super(name, birthDate, endDate);
+        this.employeeId = employeeId;
+        this.hireDate = hireDate;
+    }
+}
+
+
+class SalariedEmployee extends Employee{
+    private double annualSalary;
+    private boolean isRetired;
+
+    public void retire(){
+        System.out.println("Employee" + super.getName() + "Retired");
+    }
+
+    public SalariedEmployee(String name, String birthDate, String endDate, long employeeId, String hireDate, double annualSalary, boolean isRetired) {
+        super(name, birthDate, endDate, employeeId, hireDate);
+        this.annualSalary = annualSalary;
+        this.isRetired = isRetired;
+    }
+
+    @Override
+    public String toString() {
+        return "SalariedEmployee{" +
+                "annualSalary=" + annualSalary +
+                ", isRetired=" + isRetired +
+                ", name='" + name + '\'' +
+                "} " + super.toString();
     }
 }
